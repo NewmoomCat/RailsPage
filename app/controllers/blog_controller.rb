1a8655dcb7982c64a954b79354b729949cb1cb75
XinYueNeko <3649745663@qq.com>
2026-09-16 20:24:28 +0800

    add some

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,11 @@
 class BlogController < ApplicationController
   def blogs
+    @blog_page_title = "Neko's Blog"
+    @list_text = "Blog List: "
+    render 'blog/blogs'
+  end
+  def neko
+    @neko_page_title = "XinYueNeko's Page"
+    render 'blog/neko'
   end
 end

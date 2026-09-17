29fa0d4131e496a61268efa5021485a583b57ad9
XinYueNeko <3649745663@qq.com>
2026-09-17 17:55:53 +0800

    update some

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,11 +2,13 @@ class BlogController < ApplicationController
   def blogs
     @blog_page_title = "Neko's Blog"
     @list_text = "Blog List: "
+    @name = "XinYueNeko"
     render 'blog/blogs'
   end
   def neko
     @neko_page_title = "XinYueNeko's Page"
     @neko_h1_title = "Personal Page"
+    @name = "XinYueNeko"
     render 'blog/neko'
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   def index
     @project_page = "Neko's Project"
+    @name = "XinYueNeko"
+    render 'projects/index'
   end
 end
